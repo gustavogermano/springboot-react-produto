9e6dd808ce58a547ca4f8563ebe5bcfb6befca8f
gustavogermano <prestongu92@gmail.com>
2022-10-27 16:33:50 -0300

    Criado a rota para listar produtos

--- src/main/java/br/com/api/produtos/servico/ProdutoServico.java
+++ src/main/java/br/com/api/produtos/servico/ProdutoServico.java
@@ -1,10 +1,12 @@
 package br.com.api.produtos.servico;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import br.com.api.produtos.modelo.ProdutoModelo;
 import br.com.api.produtos.repositorio.ProdutoRepositorio;
 
+@Service
 public class ProdutoServico {
     
     @Autowired
