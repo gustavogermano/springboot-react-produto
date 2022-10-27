50131f64f2bb9e7ef9155820aa4e51f4c2159a48
gustavogermano <prestongu92@gmail.com>
2022-10-27 16:25:41 -0300

    Função listar produto no ProdutoServico

--- src/main/java/br/com/api/produtos/servico/ProdutoServico.java
+++ src/main/java/br/com/api/produtos/servico/ProdutoServico.java
@@ -1,5 +1,16 @@
 package br.com.api.produtos.servico;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.api.produtos.modelo.ProdutoModelo;
+import br.com.api.produtos.repositorio.ProdutoRepositorio;
+
 public class ProdutoServico {
     
+    @Autowired
+    private ProdutoRepositorio pr;
+
+    public Iterable<ProdutoModelo> listar(){
+        return pr.findAll();
+    }
 }
