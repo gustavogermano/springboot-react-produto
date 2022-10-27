7bfd295a0465d94c3223d683aeddbf8ac1199013
gustavogermano <prestongu92@gmail.com>
2022-10-27 17:01:58 -0300

    Função remover e requisição DELETE remover

--- src/main/java/br/com/api/produtos/servico/ProdutoServico.java
+++ src/main/java/br/com/api/produtos/servico/ProdutoServico.java
@@ -41,5 +41,13 @@ public class ProdutoServico {
         }
     }
 
+    //Método para remover produtos
+    public ResponseEntity<RespostaModelo> remover(long codigo){
+        pr.deleteById(codigo);
+
+        rm.setMensagem("O produto foi removido com sucesso!");
+        return new ResponseEntity<RespostaModelo>(rm, HttpStatus.OK);
+    }
+
 
 }
