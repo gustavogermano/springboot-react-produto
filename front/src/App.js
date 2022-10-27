86999d898257f120b1f522a6dc873e1f477d2ad2
gustavogermano <prestongu92@gmail.com>
2022-10-27 19:18:37 -0300

    Cadastrar produto

--- front/src/App.js
+++ front/src/App.js
@@ -29,12 +29,33 @@ function App() {
   const aoDigitar = (e) => {
     setObjProduto({...objProduto, [e.target.name]:e.target.value})
   }
+
+  //Cadastrar produto
+  const cadastrar = () => {
+    fetch("http://localhost:8080/cadastrar", {
+      method:'post',
+      body:JSON.stringify(objProduto),
+      headers:{
+        'Content-type':'application/json',
+        'Accept':'application/json'
+      }
+    })
+    .then(retorno => retorno.json())
+    .then(retorno_convertido => {
+      
+      if(retorno_convertido.mensagem != undefined){
+        alert(retorno_convertido.mensagem);
+      }else{
+        setProdutos([...produtos, retorno_convertido]);
+        alert('Produto cadastrado com sucesso!');
+      }
+    })
+  }
   
   //Retorno
   return (
     <div>
-      <p>{JSON.stringify(objProduto)}</p>
-      <Formulario botao={btnCadastrar} eventoTeclado={aoDigitar}/>
+      <Formulario botao={btnCadastrar} eventoTeclado={aoDigitar} cadastrar={cadastrar}/>
       <Tabela vetor={produtos}/>
     </div>
   );
