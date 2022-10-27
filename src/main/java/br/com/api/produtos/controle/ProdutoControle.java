147578cee5fee70f599865ce21914da200b5b63b
gustavogermano <prestongu92@gmail.com>
2022-10-27 16:20:07 -0300

    Configurando o ProdutoControle

--- src/main/java/br/com/api/produtos/controle/ProdutoControle.java
+++ src/main/java/br/com/api/produtos/controle/ProdutoControle.java
@@ -1,5 +1,13 @@
 package br.com.api.produtos.controle;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //responsavel por criar rotas
 public class ProdutoControle {
     
+    @GetMapping("/")
+    public String rota(){
+        return "Api de produtos funcionando!";
+    }
 }
