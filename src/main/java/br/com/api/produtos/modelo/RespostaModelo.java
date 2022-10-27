68784e059cf83f49b5142e3681d5e079b1a6396c
gustavogermano <prestongu92@gmail.com>
2022-10-27 16:23:09 -0300

    Configurando RespostaModelo

--- src/main/java/br/com/api/produtos/modelo/RespostaModelo.java
+++ src/main/java/br/com/api/produtos/modelo/RespostaModelo.java
@@ -1,5 +1,14 @@
 package br.com.api.produtos.modelo;
 
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Component //usado para o spring realizar a injeção de dependencias, sem o desenvolvedor precisar instanciar objetos
+@Getter
+@Setter
 public class RespostaModelo {
     
+    private String mensagem;
 }
