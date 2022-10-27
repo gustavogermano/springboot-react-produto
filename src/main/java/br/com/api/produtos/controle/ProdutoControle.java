e2cab05934d0cc567f603916b26cc7e4334c02d3
gustavogermano <prestongu92@gmail.com>
2022-10-27 16:53:37 -0300

    Função alterar e requisição PUT alterar

--- src/main/java/br/com/api/produtos/controle/ProdutoControle.java
+++ src/main/java/br/com/api/produtos/controle/ProdutoControle.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,9 +17,14 @@ public class ProdutoControle {
     @Autowired
     private ProdutoServico ps;
     
+    @PutMapping("/alterar")
+    public ResponseEntity<?> alterar(@RequestBody ProdutoModelo pm){
+        return ps.cadastrarAlterar(pm, "alterar");
+    }
+
     @PostMapping("/cadastrar")
     public ResponseEntity<?> cadastrar(@RequestBody ProdutoModelo pm){
-        return ps.cadastrar(pm);
+        return ps.cadastrarAlterar(pm, "cadastrar");
     }
     
     @GetMapping("/listar")
