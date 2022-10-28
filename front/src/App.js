4d14905c0e4c48834ddc96b15c70983b884e8a2e
gustavogermano <prestongu92@gmail.com>
2022-10-28 18:46:10 -0300

    Acrescentado alteração

--- front/src/App.js
+++ front/src/App.js
@@ -53,6 +53,42 @@ function App() {
     })
   }
 
+  //alterar produto
+  const alterar = () => {
+    fetch("http://localhost:8080/alterar", {
+      method:'put',
+      body:JSON.stringify(objProduto),
+      headers:{
+        'Content-type':'application/json',
+        'Accept':'application/json'
+      }
+    })
+    .then(retorno => retorno.json())
+    .then(retorno_convertido => {
+      
+      if(retorno_convertido.mensagem != undefined){
+        alert(retorno_convertido.mensagem);
+      }else{
+        alert('Produto alterado com sucesso!');
+
+          //Cópia do vetor de produtos
+        let vetorTemp = [...produtos];
+
+        let indice = vetorTemp.findIndex((p) => {
+          return p.codigo === objProduto.codigo;
+        });
+
+        //Alterar produto do vetorTemp
+        vetorTemp[indice] = objProduto;
+
+        //Atualizar vetor de produtos
+        setProdutos(vetorTemp);
+
+        limparFormulario();
+      }
+    })
+  }
+
   //Remover produto
   const remover = () => {
     fetch("http://localhost:8080/remover/"+objProduto.codigo, {
@@ -99,7 +135,7 @@ function App() {
   //Retorno
   return (
     <div>
-      <Formulario botao={btnCadastrar} eventoTeclado={aoDigitar} cadastrar={cadastrar} obj={objProduto} cancelar={limparFormulario} remover={remover}/>
+      <Formulario botao={btnCadastrar} eventoTeclado={aoDigitar} cadastrar={cadastrar} obj={objProduto} cancelar={limparFormulario} remover={remover} alterar={alterar}/>
       <Tabela vetor={produtos} selecionar={selecionarProduto}/>
     </div>
   );
